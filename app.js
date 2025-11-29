7f32eac093f17ba09d332e92f85550e317929804
vaxwe <T00258226@mymtu.ie>
2025-11-29 01:29:00 +0000

    Added Angular to project

--- app.js
+++ app.js
@@ -4,45 +4,82 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+
 const apiRoutes = require('./app_api/routes/index');
 const serverRoutes = require('./app_server/routes/index');
 
-
 require('./app_api/models/db');
-
-
 
 const app = express();
 
-// view engine setup
+// ---------------- CORS (потрібно тільки якщо фронт на 4200) ----------------
+app.use((req, res, next) => {
+  // Якщо ти все ще тестуєш Angular через ng serve на 4200:
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+// ---------------- View engine (Pug) ----------------
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// ---------------- Middleware ----------------
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Статичні файли з public (css, images і т.д.)
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', serverRoutes);
+// Статичні файли з app_public
+app.use(express.static(path.join(__dirname, 'app_public')));
+
+// 🔹 Статичні файли зібраного Angular
+app.use(
+  express.static(
+    path.join(__dirname, 'app_public')
+  )
+);
+
+// ---------------- API ----------------
 app.use('/api', apiRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// ---------------- Server-rendered сторінки (Pug) ----------------
+app.use('/', serverRoutes);
+
+// ---------------- Angular SPA (/) ----------------
+app.get('/', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'app_public')
+  );
+});
+
+
+// ---------------- 404 handler ----------------
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// ---------------- Error handler ----------------
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error =
+    req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
